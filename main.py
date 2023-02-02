f93eacc767169cdee3096987c85f43a124a9b3e0
Paul Vincent <paulvincent@oca-uk.com>
2023-02-02 15:36:03 +0000

    Files Added from Replit archive.
    Initial Commit of base files.

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.app import app
+
+
+if __name__ == '__main__':
+  app.run( # Starts the site
+  		host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
+  		port=5000  # Randomly select the port the machine hosts on.
+  	)
